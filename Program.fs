b451957e1f989ec2470f4f297d2c2796f494198c
Christopher <chrispritchard.nz@gmail.com>
2018-11-03 16:19:11 +1300

    switched to graphics instead of coloured squares

--- Program.fs
+++ Program.fs
@@ -22,30 +22,43 @@ let main _ =
     let config = {
         clearColour = Some Color.Black
         fpsFont = None
-        assetsToLoad = []
+        assetsToLoad = [
+            Texture ("tile", "tile.png")
+            Texture ("office", "office.png")
+        ]
         resolution = Windowed (800, 800)
     }
 
-    let tileSize = 40
-    let rectFor x y = x * tileSize, y * tileSize, tileSize, tileSize
+    let tileSize = 32, 24
+    let basey = 400
+
+    let rectFor x y = 
+        let halfw = (fst tileSize) / 2
+        let posx = (x * halfw) + (y * halfw);
+        let halfy = (snd tileSize) / 2
+        let posy = (y * halfy) - (x * halfy)
+        posx, basey + posy, fst tileSize, snd tileSize
     let colourFor = function "red" -> Color.Red | _ -> Color.Blue
 
     let advanceModel runState _ = 
         if wasJustPressed Keys.Escape runState then None else Some market
     
     let getView _ m =
-        let buildingLocs = m.buildings |> List.map (fun b -> b.x, b.y)
-        [1..m.width] |> List.collect (fun x -> 
-            [1..m.height] |> List.map (fun y ->
-
-                let rect = rectFor x y
-                if List.contains (x, y) buildingLocs then 
-                    Colour (rect, Color.Gray)
-                else
+        let tiles =
+            [1..m.width] |> List.collect (fun x -> 
+                [1..m.height] 
+                |> List.where (fun y -> List.tryFind (fun o -> o.x = x && o.y = y) m.buildings = None)
+                |> List.map (fun y ->
+                    let rect = rectFor x y
                     match owner x y m with
-                    | None -> Colour (rect, Color.Black)
-                    | Some o -> Colour (rect, colourFor o)
-            ))
+                    | None -> Image ("tile", rect, Color.White)
+                    | Some o -> Image ("tile", rect, colourFor o)
+                ))
+        let offices = m.buildings |> List.map (fun o -> 
+            let (ox, oy, ow, oh) = rectFor o.x o.y
+            let officeRect = ox, oy - ((snd tileSize) * 3), ow, oh * 4
+            Image ("office", officeRect, colourFor o.owner))
+        tiles @ offices
 
     runGame config advanceModel getView
     0
