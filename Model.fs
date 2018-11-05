388280c1cb5c6546359fadf6602ba4c730d39e01
Christopher <chrispritchard.nz@gmail.com>
2018-11-05 19:00:50 +1300

    Added validation for move executive

--- Model.fs
+++ Model.fs
@@ -16,7 +16,7 @@ and Building = {
     y: int
     departments: Department list
 }
-and Department = Product of quality: int | Marketing | Research of QA: bool | Executive of Executive
+and Department = Product of quality: int | Marketing | Research of QA: bool | Executive of Executive option
 and Executive = {
     name: string
     owner: Player
@@ -25,13 +25,17 @@ and Executive = {
 }
 and ExecutiveLevel = | Junior = 1 | Intermediate = 2 | Senior = 3
 and Order = { target: Building; orderType: OrderType }
-and OrderType = BuildDepartment of Department
+and OrderType = BuildDepartment of Department | MoveExecutive
 
 let validateOrder orderType target = 
     match orderType with
     | BuildDepartment _ -> 
         if target.departments.Length = maxDepartments then Error "max departments reached"
         else Ok ()
+    | MoveExecutive ->
+        match List.tryFind (function (Executive None) -> true | _ -> false) target.departments with
+        | Some _ -> Ok ()
+        | _ -> Error "no empty executive department"
 
 let issueOrder order executive = 
     if executive.orders.Length > int executive.level then Error "too many orders"
