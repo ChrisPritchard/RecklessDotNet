a1488b311a926d7550313fca4b0ab769280cd6c2
Christopher <chrispritchard.nz@gmail.com>
2018-11-05 18:44:50 +1300

    started work on an order system

--- Model.fs
+++ Model.fs
@@ -18,7 +18,16 @@ and Building = {
 and Department = Product of quality: int | Marketing | Research of QA: bool | Executive of Executive
 and Executive = {
     name: string
+    owner: Player
+    level: ExecutiveLevel
     orders: Order list
 }
-and Order = BuildDeparment of Building * Department
+and ExecutiveLevel = | Junior = 1 | Intermediate = 2 | Senior = 3
+and Order = { target: Building; orderType: OrderType }
+and OrderType = BuildDepartment of Department
 
+let issueOrder order executive = 
+    if executive.orders.Length > int executive.level then Error "too many orders"
+    elif order.target.owner <> executive.owner then Error "un-owned target"
+    // TODO: validate order
+    else Ok { executive with orders = order::executive.orders }
