9d2f1cdbf3b073495f254956af2132bb0c7da637
Christopher <chrispritchard.nz@gmail.com>
2018-11-05 19:30:29 +1300

    Implemented order processing for current orders

--- Orders.fs
+++ Orders.fs
@@ -8,9 +8,10 @@ let validateOrder orderType target =
     | BuildDepartment _ -> 
         if target.departments.Length = maxDepartments then Error "max departments reached"
         else Ok ()
-    | MoveExecutive ->
-        if List.contains (TopFloor None) target.departments 
-        then Ok () else Error "no empty executive department"
+    | MoveExecutive executive ->
+        if List.contains (TopFloor None) target.departments |> not then Error "no empty executive department"
+        elif executive.owner <> target.owner then Error "un-owned executive"
+        else Ok ()
 
 let issueOrder order executive = 
     if executive.orders.Length > int executive.level then Error "too many orders"
@@ -20,14 +21,21 @@ let issueOrder order executive =
         | Ok () -> Ok { executive with orders = order::executive.orders }
         | Error s -> Error <| sprintf "invalid order: %s" s
 
+let rec private applyOrder market order = 
+    match order.orderType with
+    | BuildDepartment newDepartment ->
+        let newBuildings =
+            market.buildings |> List.map (fun ob -> 
+                if ob = order.target then { ob with departments = newDepartment::ob.departments }
+                else ob)
+        { market with buildings = newBuildings }
+    | MoveExecutive executive ->
+        let currentBuilding = market.buildings |> List.find (fun b -> List.contains (TopFloor (Some executive)) b.departments)
+        let withoutTopFloor = List.except [(TopFloor (Some executive))] currentBuilding.departments
+        let newCurrentBuilding = { currentBuilding with departments = (TopFloor None)::withoutTopFloor }
+        let withoutCurrentBuilding = List.except [currentBuilding] market.buildings
+        let newMarket = { market with buildings = newCurrentBuilding::withoutCurrentBuilding }
+        applyOrder newMarket { order with orderType = BuildDepartment (TopFloor (Some executive)) }
+
 let processOrders executive market = 
-    executive.orders |> List.fold (fun marketDelta o -> 
-        match o.orderType with
-        | BuildDepartment newDepartment -> 
-            let newBuildings =
-                marketDelta.buildings |> List.map (fun ob -> 
-                    if ob = o.target then { ob with departments = newDepartment::ob.departments }
-                    else ob)
-            { marketDelta with buildings = newBuildings }
-        )
-        market
+    executive.orders |> List.fold applyOrder market
