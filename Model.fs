81e52fd6059e228ac48f1c9be8d6bb51811d8447
Christopher <chrispritchard.nz@gmail.com>
2018-11-06 17:33:50 +1300

    concept of used applied to research and marketing

--- Model.fs
+++ Model.fs
@@ -1,10 +1,7 @@
 module Model
 
-open Microsoft.Xna.Framework
-
 type Player = {
     name: string
-    //colour: Color
     cash: int
     ideas: int
 }
@@ -19,18 +16,9 @@ and Building = {
     x: int
     y: int
     departments: Department list
-    quality: int
 }
-and Department = Product | Marketing | Research | TopFloor
-
-type Executive = {
-    name: string
-    building: Building
-    level: ExecutiveLevel
-    orders: Order list
-}
-and ExecutiveLevel = | Junior = 1 | Intermediate = 2 | Senior = 3
-and Order = 
-    | BuildDepartment of Department * Building 
-    | DownsizeDepartment of Building * Department
-    | CreateIdea of Department
+and Department = 
+    | Product of quality: int 
+    | Marketing of used: bool 
+    | Research of used: bool 
+    | Admin
