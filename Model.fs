63f94ceae8854fa846c8ef8b5082cba9dfe4857b
Christopher <chrispritchard.nz@gmail.com>
2018-11-27 13:10:34 +1300

    Reducing model, along with some structure experiements

--- Model.fs
+++ Model.fs
@@ -4,15 +4,9 @@ type Player = {
     name: string
     cash: int
     ideas: int
-}
-
-type Market = {
-    width: int
-    height: int
     buildings: Building list
 }
 and Building = {
-    owner: Player
     x: int
     y: int
     departments: Department list
@@ -21,4 +15,16 @@ and Department =
     | Product of quality: int 
     | Marketing of used: bool 
     | Research of used: bool 
-    | Admin
+    | Acquisitions of used: bool 
+
+let advanceBuilding building =
+    let hasResearch = 
+        building.departments 
+        |> Seq.tryFind (function | Research _ -> true | _ -> false)
+        |> function | Some _ -> true | _ -> false
+    let newDepartments = 
+        building.departments 
+        |> List.map (function
+        | Product p when not hasResearch -> Product (max (p - 10) 10)
+        | d -> d)
+    { building with departments = newDepartments }
