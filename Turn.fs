743845f6c74875bfe350d1ec90b9d42a71920846
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-01-07 14:22:14 +1300

    made tile collector recursive

--- Turn.fs
+++ Turn.fs
@@ -2,7 +2,7 @@ module Turn
 
 open Model
 
-let productTiles office parentMarketing = 
+let rec productTiles parent office = 
     let products, quality, localMarketing = 
         office.departments 
         |> List.fold (fun (p, q, m) -> 
@@ -10,13 +10,20 @@ let productTiles office parentMarketing =
             | Product v -> p + 1, q + v, m
             | Marketing -> p, q, m + 1
             | _ -> p, q, m) (0, 0, 0)
+    let parentMarketing = 
+        match parent with 
+        | Some o -> o.departments |> List.sumBy (function Marketing -> 1 | _ -> 0) 
+        | _ -> 0
     let quality = quality * pown 2 (localMarketing + parentMarketing)
     
-    [-products..products] |> List.collect (fun x ->
-    [-products..products] |> List.map (fun y -> x, y))
-    |> List.filter (fun (x, y) -> abs x + abs y <= products)
-    |> List.map (fun (x, y) -> office.x + x, office.y + y, quality)
+    let localTiles =
+        [-products..products] |> List.collect (fun x ->
+        [-products..products] |> List.map (fun y -> x, y))
+        |> List.filter (fun (x, y) -> abs x + abs y <= products)
+        |> List.map (fun (x, y) -> office.x + x, office.y + y, quality)
     
+    localTiles @ List.collect (productTiles (Some office)) office.managedOffices
+
 let updateQuality office researchInUse =
     let hasResearch = List.contains Research office.departments
     let qaCount = office.extensions |> List.sumBy (fun e -> match e with QA -> 1)
