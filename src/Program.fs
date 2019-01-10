4c1e8af66fe7d41e376434ad9de0cffe3babc3bf
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-01-10 13:59:17 +1300

    added basic gamecore game added a ps and sh script to run sub dir easily

--- src/Program.fs
+++ src/Program.fs
@@ -2,6 +2,10 @@
 open Model
 open Turn
 
+open Microsoft.Xna.Framework.Input
+open GameCore.GameRunner
+open GameCore.GameModel
+
 [<EntryPoint>]
 let main _ =
 
@@ -15,6 +19,17 @@ let main _ =
 
     updateQuality test [] |> fun o -> printfn "%A" o.departments
     
+    let startModel = ()
 
+    let advanceModel runState model =
+        if wasJustPressed Keys.Escape runState then None
+        else
+            match model with
+            | None -> Some startModel
+            | _ -> model
+    
+    let getView _ _ = []
+
+    runWindowedGame (800, 600) [] advanceModel getView
 
     0
