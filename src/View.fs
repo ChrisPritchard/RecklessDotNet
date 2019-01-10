30d203266d0e1c059c123c4081135fdc3dd95ebd
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-01-10 16:00:58 +1300

    tile centreing based on res

--- src/View.fs
+++ src/View.fs
@@ -6,7 +6,10 @@ open Model
 
 let tw, th = Constants.tileSize
 
-let iso x y cx cy = 
+let rx, ry = Constants.windowSize
+let cx, cy = (rx - (tw * 10)) / 2 + (tw/2), ry / 2 + (th/2)
+
+let iso x y = 
     cx + (x * tw/2 + y*tw/2), cy + (y*th/2 - x*th/2)
 
 let bottomCentre x y w h =
@@ -15,13 +18,13 @@ let bottomCentre x y w h =
 let getView runState (map, (corps: Corporation list)) = [
     yield! 
         Set.toList map |> List.map (fun (x, y) -> 
-            let ix, iy = iso x y 96 300
+            let ix, iy = iso x y
             let ix, iy = bottomCentre ix iy tw th
             let destRect = ix, iy, tw, th
             Image ("tile", destRect, Color.White))
     yield! 
         corps |> List.map (fun c -> 
-            let ix, iy = iso c.headOffice.x c.headOffice.y 96 300
+            let ix, iy = iso c.headOffice.x c.headOffice.y
             let ix, iy = bottomCentre ix iy tw (th*3)
             let destRect = ix, iy, tw, th*3
             Image ("office", destRect, c.colour))
