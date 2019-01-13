5efc3aa86149e5c99cdc471c8a07f7a6b975067c
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-01-14 11:10:53 +1300

    updated to latest versions of nuget packages. fixed up mousepos display

--- src/UI.fs
+++ src/UI.fs
@@ -2,35 +2,26 @@ module Reckless.UI
 
 open Microsoft.Xna.Framework
 open GameCore.GameModel
-open GameCore.UIElements.Model
-open GameCore.UIElements.Functions
+open GameCore.UIElements.Common
+open GameCore.UIElements.Button
 
-// let standardColours = { background = Color.DarkBlue; border = Some (3, Color.Blue); text = Color.White }
+let updateUIModel _ ui = ui
+    
+let mousePos runState = { 
+    text = [sprintf "%i, %i" <|| runState.mouse.position]
+    fontAsset = "font"
+    fontSize = 16
+    destRect = (10, 10, 140, 40)
+    idleColours = { background = Color.DarkBlue; border = Some (3, Color.Blue); text = Color.White }
+    hoverColours = None
+    pressedColours = None
+    state = []
+    }
 
-// let table destRect header rows = 
-//     Panel ({
-//         destRect = destRect
-//         background = Some standardColours.background
-//         border = standardColours.border
-//         padding = Some 10
-//         alignment = Some (AlignVertically 10)
-//     }, [ 
-//         Label {  }
-//         ])
+let getUIView runState ui = [
+    yield! getButtonView <| mousePos runState
 
-let updateUIModel runState ui =
-    [
-        yield Label { 
-            text = [sprintf "%i, %i" <|| runState.mouse.position]
-            fontAsset = "font"
-            destRect = (10, 10, 100, 40)
-            colours = { background = Color.DarkBlue; border = Some (3, Color.Blue); text = Color.White }
-            }
-    ]
-
-let getUIView runState ui = 
-    let nextUI = List.collect (getElementView runState) ui
     let isPressed = isMousePressed (true, false) runState
     let mx, my = runState.mouse.position 
-    nextUI @ [Colour ((mx, my, 5, 5), (if isPressed then Color.Red else Color.Yellow))]
-    
+    yield Colour ((mx, my, 5, 5), (if isPressed then Color.Red else Color.Yellow))
+]
