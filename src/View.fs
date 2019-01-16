d87d2a11ba2c08f80ad07a6b89717528a185b728
Christopher <chrispritchard.nz@gmail.com>
2019-01-17 08:45:07 +1300

    extracted view methods into their own funcs

--- src/View.fs
+++ src/View.fs
@@ -9,6 +9,39 @@ open Turn
 
 let tw, th = Constants.tileSize
 
+let private findProductTiles corps =
+    corps
+    |> List.collect (fun c -> 
+            productTiles None c.headOffice
+            |> List.map (fun (x, y, q) -> (x, y), (c, q)))
+    |> List.groupBy fst
+    |> List.map (fun (pos, tiles) -> 
+        let best = tiles |> List.sortByDescending (fun (_, (_, q)) -> q) |> List.head |> snd
+        pos, best)
+    |> Map.ofList
+
+let private renderMap productTiles (mx, my) map =
+    map
+    |> Set.toList
+    |> List.collect (fun (x, y) -> [
+            let rect = isoRect x y tw th
+            match Map.tryFind (x, y) productTiles with
+            | Some (c, _) -> yield 0, Image ("tile", rect, c.colour)
+            | None -> yield 0, Image ("tile", rect, Color.White)
+            if (x, y) = (mx, my) then
+                yield 2, Image ("tile-highlight", rect, Color.White)
+        ])
+
+let private renderOffices (mx, my) corps =
+    corps
+    |> List.collect (fun corp -> [
+            let o = corp.headOffice
+            let rect = isoRect o.x o.y tw (th*3)
+            yield 1, Image ("office", rect, corp.colour)
+            if (o.x, o.y) = (mx, my) then
+                yield 3, Image ("office-highlight", rect, Color.White)
+        ])
+
 let getView runState (map, (corps: Corporation list), ui) = 
     [
         // render map and corps
@@ -17,40 +50,10 @@ let getView runState (map, (corps: Corporation list), ui) =
         // render status (cash, cashflow, ideas)
         // render mouse cursor
 
-    let mx, my = mouseTile runState |> Option.defaultValue (-1, -1)
-
-    let productTiles = 
-        corps 
-        |> List.collect (fun c -> 
-            productTiles None c.headOffice
-            |> List.map (fun (x, y, q) -> (x, y), (c, q)))
-        |> List.groupBy fst
-        |> List.map (fun (pos, tiles) -> 
-            let best = tiles |> List.sortByDescending (fun (_, (_, q)) -> q) |> List.head |> snd
-            pos, best)
-        |> Map.ofList
-
-    yield! 
-        Set.toList map 
-        |> List.collect (fun (x, y) -> [
-            let rect = isoRect x y tw th
-            match Map.tryFind (x, y) productTiles with
-            | Some (c, _) -> yield 0, Image ("tile", rect, c.colour)
-            | None -> yield 0, Image ("tile", rect, Color.White)
-            if (x, y) = (mx, my) then
-                yield 2, Image ("tile-highlight", rect, Color.White)
-            ])
-
-    yield! 
-        corps 
-        |> List.collect (fun corp -> [
-            let o = corp.headOffice
-            let rect = isoRect o.x o.y tw (th*3)
-            yield 1, Image ("office", rect, corp.colour)
-            if (o.x, o.y) = (mx, my) then
-                yield 3, Image ("office-highlight", rect, Color.White)
-            ])
-  
+        let mousePos = mouseTile runState |> Option.defaultValue (-1, -1)
+        let productTiles = findProductTiles corps
+        yield! renderMap productTiles mousePos map
+        yield! renderOffices mousePos corps
     ] 
     |> List.sortBy fst 
     |> List.map snd 
