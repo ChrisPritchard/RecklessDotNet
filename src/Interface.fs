4f8994f28ffc6ab0e38c87bfe3c1039d28631877
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-02-01 19:53:31 +1300

    interim work implementing popups working except for player corp

--- src/Interface.fs
+++ src/Interface.fs
@@ -6,46 +6,6 @@ open GameCore.ImGui.Wrappers
 open Model
 open Constants
     
-//let private tilePopup corpList =
-//    let qualityLine (corp, quality) =
-//        sprintf "%s: %i" corp.abbreviation quality
-//    let head = List.head corpList |> qualityLine
-//    let rest = match corpList with | _::rest -> rest |> List.map qualityLine | _ -> []
-//    popup head rest
-
-//let private officePopup office corp =
-//    let corpName = sprintf "%s (%s)" corp.name corp.abbreviation
-//    let departments = office.departments |> List.map (sprintf "%A")
-//    popup corpName departments
-
-//let private findOfficePopup (mx, my) gameState = 
-//    let office = 
-//        allCorps gameState
-//        |> List.collect (fun c -> allOffices c.headOffice |> List.map (fun o -> o, c))
-//        |> List.tryFind (fun (o, _) -> o.x = mx && o.y = my)
-//    office |> Option.bind (fun (o, c) ->
-//        Some (officePopup o c))
-
-//let private findTilePopup (mx, my) productTiles = 
-//    match Map.tryFind (mx, my) productTiles with
-//    | Some corpList -> tilePopup corpList
-//    | _ -> []
-
-//let renderInterface productTiles gameState = 
-//    [
-//        yield! playerStats productTiles gameState
-
-//        match gameState.selectedTile with
-//        | Some (mx, my) ->
-//            let popup = 
-//                findOfficePopup (mx, my) gameState 
-//                |> Option.defaultValue (findTilePopup (mx, my) productTiles)
-//            yield! popup
-//        | _ -> ()
-
-//        yield! getButtonView gameState.buttons.endTurn
-//    ]
-
 type UIModel = {
     endTurn: bool
 }
@@ -66,8 +26,7 @@ let window label pos size (flags: ImGuiWindowFlags) children =
         ImGui.End ()
         next
 
-let playerStats gameState = 
-    let productTiles = gameProductTiles gameState
+let playerStats productTiles gameState = 
     let income = Map.find gameState.player (incomeByCorp productTiles productIncome)
     let expenses = Map.find gameState.player (expensesByCorp departmentCost gameState)
 
@@ -82,6 +41,32 @@ let playerStats gameState =
         text (sprintf "Orders Remaining     %i" (2 - gameState.player.orders.Length))
     ]
 
+let popupFlags = 
+    ImGuiWindowFlags.NoResize ||| ImGuiWindowFlags.NoMove 
+    ||| ImGuiWindowFlags.NoCollapse ||| ImGuiWindowFlags.AlwaysAutoResize
+    ||| ImGuiWindowFlags.NoScrollbar ||| ImGuiWindowFlags.NoInputs
+    ||| ImGuiWindowFlags.NoFocusOnAppearing
+
+let showPopup (mx, my) productTiles gameState = 
+    allCorps gameState
+    |> List.collect (fun c -> allOffices c.headOffice |> List.map (fun o -> o, c))
+    |> List.tryFind (fun (o, _) -> o.x = mx && o.y = my)
+    |> function
+    | Some (office, corp) ->
+        let corpName = sprintf "%s (%s)" corp.name corp.abbreviation
+        window corpName (Some (10, 10)) (Some (150, 150)) popupFlags [
+            yield! List.map (fun d -> text (sprintf "%A" d)) office.departments
+        ]
+    | None -> 
+        match Map.tryFind (mx, my) productTiles with
+        | Some corpList -> 
+            let qualityLine (corp, quality) =
+                sprintf "%s: %i" corp.abbreviation quality
+            window "" (Some (10, 10)) None (popupFlags ||| ImGuiWindowFlags.NoTitleBar) [
+                yield! List.map (fun c -> text (qualityLine c)) corpList
+            ]
+        | _ -> fun m _ -> m
+
 let getInterface (gameState: GameState) =
     { endTurn = false },
     [
@@ -89,8 +74,12 @@ let getInterface (gameState: GameState) =
             ImGui.StyleColorsLight ()
             uimodel
         )
-
-        yield playerStats gameState
+        
+        let productTiles = gameProductTiles gameState
+        yield playerStats productTiles gameState
+        match gameState.selectedTile with
+        | Some point -> yield showPopup point productTiles gameState
+        | None -> ()
 
         let endTurnFlags = flags ||| ImGuiWindowFlags.NoTitleBar ||| ImGuiWindowFlags.NoBackground ||| ImGuiWindowFlags.AlwaysAutoResize
         yield window "" (Some (winw - 110, winh - 60)) None endTurnFlags [
