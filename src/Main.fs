72bb7ae38b33ba6fa8526303b45726b3ff80c84c
Christopher <chrispritchard.nz@gmail.com>
2019-05-09 12:34:14 +1200

    fixed up highlighting issue

--- src/Main.fs
+++ src/Main.fs
@@ -67,21 +67,23 @@ let private renderOffices gameState =
         let (x, y, w, h) = isoRect office.x office.y tileWidth (tileHeight*3)
         image "office" corp.colour (w, h) (x, y))
 
-let private renderHighlight gameState (x, y) = [
-        let (x, y, w, h) = isoRect x y tileWidth tileHeight
+let private renderHighlight gameState (mx, my) = [
+        let (x, y, w, h) = isoRect mx my tileWidth tileHeight
         yield image "tile-highlight" Colour.White (w, h) (x, y)
 
         let allOffices = allCorps gameState |> List.collect (fun corp -> allOffices corp.headOffice)
-        match List.tryFind (fun office -> (office.x, office.y) = (x, y)) allOffices with
+        match List.tryFind (fun office -> (office.x, office.y) = (mx, my)) allOffices with
         | None -> ()
         | Some office ->
             let (x, y, w, h) = isoRect office.x office.y tileWidth (tileHeight*3)
             yield image "office-highlight" Colour.White (w, h) (x, y)
 
             let tiles = officeProductTiles office
-            yield! tiles |> List.filter (fun p -> Set.contains p gameState.market) |> List.map (fun (x, y) ->
-                let (x, y, w, h) = isoRect office.x office.y tileWidth (tileHeight*3)
-                image "tile-highlight" Colour.White (w, h) (x, y))
+            yield! tiles 
+                |> List.filter (fun p -> Set.contains p gameState.market) 
+                |> List.map (fun (tx, ty) ->
+                    let (x, y, w, h) = isoRect tx ty tileWidth tileHeight
+                    image "tile-highlight" Colour.White (w, h) (x, y))
     ]
 
 let view model dispatch =
