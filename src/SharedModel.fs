18740cb80f48b5676780b2bbf65e8bbee098787b
Christopher <chrispritchard.nz@gmail.com>
2019-05-10 15:45:13 +1200

    filtered back model to raw components

--- src/SharedModel.fs
+++ src/SharedModel.fs
@@ -2,13 +2,16 @@
 
 open Xelmish.Model
 
+type Executive = {
+    name: string
+}
+
 type Corporation = {
     name: string
     abbreviation: string
     cash: int
     ideas: int
     headOffice: Office
-    orders: Order list
     colour: Colour
 }
 and Office = {
@@ -16,17 +19,9 @@ and Office = {
     y: int
     managedOffices: Office list
     departments: Department list
-    extensions: Extension list
 }
 and Department =
     | Product of quality: int 
     | Marketing
     | Research
-    | Acquisitions
-and Extension =
-    | QA
-and Order =
-    | BuildDepartment of Office * Department
-    | BuildExtension of Office * Extension
-    | ResearchIdea of researchLocation:Office
-    | BuildOffice of acquisitions:Office * x:int * y:int * Department
+    | Admin of Executive option
