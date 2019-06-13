a62ea421c4ba9949a288d4b73232eeae0807327f
Christopher <chrispritchard.nz@gmail.com>
2019-06-13 16:14:08 +1200

    fixed typo

--- src/main/Orders.fs
+++ src/main/Orders.fs
@@ -16,7 +16,7 @@ let buildProductOrder = {
     baseCondition = fun corp -> corp.ideas > 0
     kind = 
         let isValid office market =
-            List.contains (fun (o, _, _) -> o = office) market.player.allOffices
+            List.exists (fun (o, _, _) -> o = office) market.player.allOffices
             && office.departments.Length < 6
         let action office =
             { office with departments = Product 100::office.departments }
@@ -26,6 +26,7 @@ let buildProductOrder = {
 // TODO:
 // base condition should check if order is valid in offices beneath executive, not whole corp?
 // office condition should be able to check ownership - do we need a new member on office?
+    // solved: add allOffices to corps, then pass market to valid office query
 // action needs to be thought through - group orders by office and apply enmass?
 
 let orderOptionsFor corporation executiveOffice = []
