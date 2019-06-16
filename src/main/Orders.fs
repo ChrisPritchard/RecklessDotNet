66a7a85d63be5e38504913aae0d3d2552e60f32c
Christopher <chrispritchard.nz@gmail.com>
2019-06-17 11:51:20 +1200

    idea notes

--- src/main/Orders.fs
+++ src/main/Orders.fs
@@ -20,6 +20,15 @@ and OrderKind =
 and OrderAction = (Corporation * Office) -> (Corporation * Office)
 and CorpOfficeIdentifier = string * (int * int)
 
+// name
+// action to run with typed variables
+// conditions to evaluate
+// name and condition/action?
+// fun (ValidOffice) -> office
+// could track order results rather than order actions...would allow precog of next turn...
+// ValidOffice -> OwnOffice of (office -> bool) * (office -> office) | OtherOffice | Corp
+// OrderAction instead of ValidOffice
+
 let buildProductOrder = {
     name = "Build Product"
     corpCondition = fun corp -> corp.ideas > 0
