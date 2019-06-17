7f14a3779cefca1686ebdc8f9569d2eb93cd4a50
Christopher <chrispritchard.nz@gmail.com>
2019-06-17 12:29:06 +1200

    revised again, to much simpler combined model

--- src/main/Orders.fs
+++ src/main/Orders.fs
@@ -4,30 +4,19 @@ open Model
 
 type Order = 
     {   name: string
-        components: (OrderSelector * OrderTransform) list }
-and OrderSelector =
-    | OfficeSelector of (Office -> bool -> bool)
-    | CorpSelector of (Corporation -> bool -> bool)
-    | Tile of ((int * int) -> bool)
-and OrderTransform =
-    | OfficeTransform of (Office -> Office)
-    | CorpTransform of (Corporation -> Corporation)
-
-// name
-// action to run with typed variables
-// conditions to evaluate
-// name and condition/action?
-// fun (ValidOffice) -> office
-// could track order results rather than order actions...would allow precog of next turn...
-// ValidOffice -> OwnOffice of (office -> bool) * (office -> office) | OtherOffice | Corp
-// OrderAction instead of ValidOffice
+        components: OrderComponent list }
+and OrderComponent =
+    | CorpTransform of condition:(Corporation -> bool -> bool) * action:(Corporation -> Corporation)
+    | OfficeTransform of condition:(Office -> bool -> bool) * action:(Office -> Office)
 
 let buildProductOrder = {
     name = "Build Product"
     components = [
-        CorpSelector (fun corp isOwn -> isOwn && corp.ideas > 0), 
-        CorpTransform (fun corp -> { corp with ideas = corp.ideas - 1 })
-        OfficeSelector (fun office isOwn -> isOwn && office.departments.Length < 6), 
-        OfficeTransform (fun office -> { office with departments = Product 100::office.departments })
+        CorpTransform (
+            (fun corp isOwn -> isOwn && corp.ideas > 0), 
+            fun corp -> { corp with ideas = corp.ideas - 1 })
+        OfficeTransform (
+            (fun office isOwn -> isOwn && office.departments.Length < 6), 
+            fun office -> { office with departments = Product 100::office.departments })
     ]
 }
