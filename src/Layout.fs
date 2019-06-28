21f91f5af146fc35df0fd6ca084145cdf6707e10
Chris Pritchard <chris.pritchard@edpay.nz>
2019-06-28 13:49:30 +1200

    Created a marginpad function

--- src/Layout.fs
+++ src/Layout.fs
@@ -1,5 +1,9 @@
 ﻿module Layout
 
+/// Divides a rectangle into rows and columns, with rows and columns given as float percentages.
+/// Ignores rows/columns that do not fix, and/or truncates them as necessary. 
+/// Also returns remainder space as a rect if the outer rect is not totally consumed.
+/// Returns a list of subrects in the same x,y,w,h tuple format as the outer rect.
 let divide (left, top, totalWidth, totalHeight) (rows: float list) (cols: float list) =
     let folder (results, remainder) segment = 
         if remainder = 0 then results, remainder
@@ -23,3 +27,9 @@ let divide (left, top, totalWidth, totalHeight) (rows: float list) (cols: float 
         Seq.mapFold (colFolder currentTop height) left colWidths |> fst, currentTop + height
 
     Seq.mapFold rowFolder top rowHeights |> fst |> Seq.collect id |> Seq.toList
+
+/// Returns a rectangle that is within the out rectangle by a margin. 
+/// Also returns a rectangle inside this first rectangle by a padding amount.
+let marginPad (left, top, width, height) margin padding =
+    (left + margin, top + margin, width - (margin * 2), height - (margin * 2)),
+    (left + margin + padding, top + margin + padding, width - ((margin + padding) * 2), height - ((margin + padding) * 2))
