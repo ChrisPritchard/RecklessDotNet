14cb2410b4f13f41b5c4916189155ba0fec6b4bb
Christopher <chrispritchard.nz@gmail.com>
2019-06-30 16:50:23 +1200

    moved button ans text into ui module

--- src/main/UI.fs
+++ src/main/UI.fs
@@ -1,5 +1,7 @@
 ﻿module Main.UI
 
+open Xelmish.Model
+open Xelmish.Viewables
 open Common
 open Layout
 
@@ -12,5 +14,22 @@ let corpInfoRect, execRect, selectedInfoRect =
     rowsAndCols controlPanelRect [] [0.3; 0.3]
     |> fun a -> a.[0], a.[1], a.[2]
 
-let orderButtonOptions = 
-    rowsAndCols controlPanelRect [0.2; 0.2; 0.2; 0.2; 0.2] [0.25; 0.25; 0.25; 0.25]
+let orderSelectPanelRect = 
+    rowsAndCols controlPanelRect [0.2; 0.16; 0.16; 0.16; 0.16; 0.16] [0.25; 0.25; 0.25; 0.25]
+
+let text = text "defaultFont" 20.
+let colours = {|
+        text = Colour.White
+        button = Colour.Blue
+        buttonDisabled = Colour.LightBlue
+        //hover = Colour.AliceBlue
+        //pressed = Colour.DarkBlue
+    |}
+
+let button (x, y, w, h) displayText action enabled =
+    let textPos = middle (x, y, w, h)
+    let backColour = if enabled then colours.button else colours.buttonDisabled
+    [   yield colour backColour (w, h) (x, y)
+        yield text colours.text (-0.5, -0.5) displayText textPos
+        if enabled then yield onclick action (w, h) (x, y) ]
+    
