cf37a6e6094d2aeb6a1c070297af9a569da0971e
Christopher <chrispritchard.nz@gmail.com>
2019-07-11 17:28:56 +1200

    interim work on list part of diplay

--- src/main/View_Interface.fs
+++ src/main/View_Interface.fs
@@ -38,21 +38,9 @@ let private rowsAndCols (rows: float list) (cols: float list) (x, y, w, h) =
     |> Seq.sortBy (fun (x, y, _, _) -> x, y) 
     |> Seq.toArray
     
-/// Simple mapping function on a rect to return its position.
-/// Use with rowsAndCols or similar if you don't care about width and height
 let private topLeft (x, y, _, _) = x, y
-
-/// Simple function to return the point at the middle of a rect.
-/// Useful when placing centre-aligned text.
+let private topRight (x, y, w, _) = x + w, y
 let private middle (x, y, w, h) = x + w / 2, y + h / 2
-
-/// Takes a rect and splits it down the middle into left and right halves, returned as an array.
-let private splitVertically (x, y, w, h) =
-    [|(x, y, w/2, h); (x + w/2, y, w/2, h)|]
-
-/// Takes a rect and splits it across the middle into top and bottom halves, returned as an array. 
-let private splitHorizontally (x, y, w, h) =
-    [|(x, y, w, h/2); (x, y + h/2, w, h/2)|]
 
 /// Returns a rectangle that is within the out rectangle by a margin. 
 /// Also returns a rectangle inside this first rectangle by a padding amount.
@@ -64,24 +52,26 @@ let contains (tx, ty) (x, y, w, h) =
     tx >= x && tx < x + w &&
     ty >= y && ty < y + h
 
-let orderSelectTabRects, orderSelectButtonRects = 
-    let tabPanel, buttonPanel =
-        rowsAndCols [0.2;0.8] [] controlPanelRect
-        |> fun a -> a.[0], a.[1]
-    let tabRects = rowsAndCols [] [0.125; 0.125; 0.125; 0.125; 0.125; 0.125; 0.125; 0.125] tabPanel
-    let buttonRects = 
-        rowsAndCols [0.25; 0.25; 0.25; 0.25] [0.25; 0.25; 0.25; 0.25] buttonPanel
-        |> Array.map (marginPad 5 0 >> fst) 
-    tabRects, buttonRects
+//let orderSelectTabRects, orderSelectButtonRects = 
+//    let tabPanel, buttonPanel =
+//        rowsAndCols [0.2;0.8] [] controlPanelRect
+//        |> fun a -> a.[0], a.[1]
+//    let tabRects = rowsAndCols [] [0.125; 0.125; 0.125; 0.125; 0.125; 0.125; 0.125; 0.125] tabPanel
+//    let buttonRects = 
+//        rowsAndCols [0.25; 0.25; 0.25; 0.25] [0.25; 0.25; 0.25; 0.25] buttonPanel
+//        |> Array.map (marginPad 5 0 >> fst) 
+//    tabRects, buttonRects
     
-let text = text "defaultFont" 20.
 let colours = {|
+        background = Colour.DarkGray
         text = Colour.White
         button = Colour.Blue
         buttonDisabled = Colour.Gray
         buttonHover = Colour.LightBlue
         buttonPressed = Colour.DarkBlue
     |}
+let normalText = text "defaultFont" 20. colours.text
+let titleText = text "defaultFont" 25. colours.text
 
 let button (x, y, w, h) displayText action enabled =
     let textPos = middle (x, y, w, h)
@@ -97,13 +87,40 @@ let button (x, y, w, h) displayText action enabled =
         yield conditionalColour colours.buttonPressed (w, h) (x, y) (fun inputs -> 
             enabled && hovering inputs && inputs.mouseState.LeftButton = ButtonState.Pressed)
 
-        yield text colours.text (-0.5, -0.5) displayText textPos
+        yield normalText (-0.5, -0.5) displayText textPos
         if enabled then 
             yield onclick action (w, h) (x, y) ]
 
-let corpInfo corporation windowRect = []
-let executiveInfo corporation dispatch windowRect = []
-let selectedInfo selected windowRect = []
+let corpInfo corporation (x, y, w, h) = [
+    let rects = 
+        rowsAndCols [0.18;0.14;0.14;0.14;0.14;0.14;0.14] [0.5;0.5] (x, y, w, h)
+        |> Array.map (marginPad 5 0 >> fst)
+
+    yield colour colours.background (w, h) (x, y)
+    yield setSmoothSampling ()
+    yield titleText (0., 0.) corporation.name (topLeft rects.[0])
+
+    let listItem i (label, value) =
+        [
+            normalText (-1., 0.) label (topRight rects.[2 + (i*2)])
+            normalText (0., 0.) value (topLeft rects.[2 + (i*2) + 1])
+        ]
+
+    yield! [
+        "Ideas", string corporation.ideas
+        "Prospects", "TODO"
+        "Stock Value", "TODO"
+        "Market Share", "TODO"
+        "Cash", sprintf "$%i" corporation.cash
+        "Expenses", "TODO"
+    ] |> List.mapi listItem |> List.collect id
+]
+let executiveInfo corporation dispatch (x, y, w, h) = [
+    yield colour colours.background (w, h) (x, y)
+]
+let selectedInfo selected (x, y, w, h) = [
+    yield colour colours.background (w, h) (x, y)
+]
 
 let informationPanels corporation selected dispatch windowRect =     
     let corpInfoRect, execRect, selectedInfoRect = 
