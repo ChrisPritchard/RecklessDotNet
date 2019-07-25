41d03eb302b61f76b342c7bc946c072c1c06da58
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-07-25 12:47:45 +1200

    created some sample types for ui project

---
+++ lib/Xelmish.UI/Model.fs
@@ -0,0 +1,28 @@
+﻿namespace Xelmish.UI.Model
+
+type Element = {
+    elementType: ElementType
+    attributes: Attribute list
+    children: Element list
+}
+and ElementType =
+    | Row
+    | Column
+    | Text
+    | Button
+and Attribute = 
+    | Style of style: Style
+    | Text of string
+    | OnClick of event: (unit -> unit)
+and Style = {
+    fontName: string
+    fontSize: float
+    colour: Colour
+    backgroundColour: Colour
+    margin: float
+    padding: float
+    borderSize: float
+    borderColour: Colour
+    alignment: float
+}
+and Colour = byte * byte * byte * byte
