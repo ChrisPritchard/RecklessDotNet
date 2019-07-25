e54d797112583105833d06ff194e5e9cd75680a1
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-07-25 13:28:43 +1200

    created creator functions and some test code

--- lib/Xelmish.UI/Model.fs
+++ lib/Xelmish.UI/Model.fs
@@ -1,28 +1,57 @@
-﻿namespace Xelmish.UI.Model
+﻿namespace Xelmish.UI
 
-type Element = {
-    elementType: ElementType
-    attributes: Attribute list
-    children: Element list
-}
-and ElementType =
-    | Row
-    | Column
-    | Text
-    | Button
-and Attribute = 
-    | Style of style: Style
-    | Text of string
-    | OnClick of event: (unit -> unit)
-and Style = {
-    fontName: string
-    fontSize: float
-    colour: Colour
-    backgroundColour: Colour
-    margin: float
-    padding: float
-    borderSize: float
-    borderColour: Colour
-    alignment: float
-}
-and Colour = byte * byte * byte * byte
+module Model =
+
+    type Element = {
+        elementType: ElementType
+        attributes: Attribute list
+        children: Element list
+    }
+    and ElementType =
+        | Row
+        | Column
+        | Span
+        | Button
+    and Attribute = 
+        | Style of style: (Style -> Style)
+        | Text of string
+        | OnClick of event: (unit -> unit)
+    and Style = {
+        fontName: string
+        fontSize: float
+        colour: Colour
+        backgroundColour: Colour
+        margin: float
+        padding: float
+        borderSize: float
+        borderColour: Colour
+        alignment: float
+    }
+    and Colour = byte * byte * byte * byte
+
+    let private shim elementType attributes children = { elementType = elementType; attributes = attributes; children = children }
+
+    let col = shim Column
+    let row = shim Row
+    let span = shim Span
+    let button = shim Button
+
+    let text s = Text s
+    let onclick f = OnClick f
+    
+    let fontName s = Style (fun style -> { style with fontName = s })
+    let fontSize s = Style (fun style -> { style with fontSize = s })
+    let colour s = Style (fun style -> { style with colour = s })
+    let backgroundColour s = Style (fun style -> { style with backgroundColour = s })
+    let margin s = Style (fun style -> { style with margin = s })
+    let padding s = Style (fun style -> { style with padding = s })
+    let borderSize s = Style (fun style -> { style with borderSize = s })
+    let borderColour s = Style (fun style -> { style with borderColour = s })
+    let alignment s = Style (fun style -> { style with alignment = s })
+
+    let testModel = 
+        col [] [
+            span [ fontSize 20.; text "Cell 1" ] []
+            span [ text "This is some sample text" ] []
+            button [ text "Click Me"; onclick (fun _ -> ()) ] []
+        ]
