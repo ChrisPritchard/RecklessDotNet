5fe01b0eaa76a1d8fb7e9a49ac4efd8e5e504e13
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-07-28 14:19:04 +1200

    interim work on pixel/percent sizes

--- lib/Xelmish.UI/Model.fs
+++ lib/Xelmish.UI/Model.fs
@@ -13,22 +13,24 @@ module Model =
         | Button of string
     and Attribute = 
         | Style of style: (Style -> Style)
-        | Width of int
-        | Height of int
+        | Width of Size
+        | Height of Size
         | OnClick of event: (unit -> unit)
     and Style = {
         fontName: string
         fontSize: float
         colour: Colour
         backgroundColour: Colour
-        margin: int
-        padding: int
+        margin: Sizes
+        padding: Sizes
         borderSize: int
         borderColour: Colour
         alignment: float
         enabled: bool
     }
     and Colour = byte * byte * byte * byte
+    and Size = Percent of float | Pixels of int
+    and Sizes = Uniform of Size | Sides of Size * Size * Size * Size
 
     let col attributes children = { elementType = Column children; attributes = attributes }
     let row attributes children = { elementType = Row children; attributes = attributes }
@@ -58,30 +60,36 @@ module Model =
             button [ enabled false ] "Can't click Me"
         ]
 
-    let rec private renderRow renderImpl left spaceRemaining childrenRemaining =
+    let rec private renderRow renderImpl left totalSpace spaceRemaining childrenRemaining =
         [
             match childrenRemaining with
             | [] -> ()
             | child::rest ->
                 let width = 
                     child.attributes 
-                    |> List.tryPick (function Width x -> Some x | _ -> None) 
+                    |> List.tryPick (function 
+                        | Width (Pixels x) -> Some x 
+                        | Width (Percent x) -> Some (x * float totalSpace |> int) 
+                        | _ -> None) 
                     |> Option.defaultValue (spaceRemaining / childrenRemaining.Length)
                 yield! renderImpl left width child
-                yield! renderRow renderImpl (left + width) (spaceRemaining - width) rest
+                yield! renderRow renderImpl (left + width) totalSpace (spaceRemaining - width) rest
         ]
     
-    let rec private renderCol renderImpl top spaceRemaining childrenRemaining =
+    let rec private renderCol renderImpl top totalSpace spaceRemaining childrenRemaining =
         [ 
             match childrenRemaining with
             | [] -> ()
             | child::rest ->
                 let height = 
                     child.attributes 
-                    |> List.tryPick (function Height x -> Some x | _ -> None) 
+                    |> List.tryPick (function 
+                        | Height (Pixels x) -> Some x 
+                        | Height (Percent x) -> Some (x * float totalSpace |> int) 
+                        | _ -> None) 
                     |> Option.defaultValue (spaceRemaining / childrenRemaining.Length)
                 yield! renderImpl top height child
-                yield! renderCol renderImpl (top + height) (spaceRemaining - height) rest 
+                yield! renderCol renderImpl (top + height) totalSpace (spaceRemaining - height) rest 
         ]
 
     let private renderText style (x, y) (width, height) text = []
@@ -92,6 +100,10 @@ module Model =
             ({ style with padding = 0; margin = 0 }, element.attributes) 
             ||> List.fold (fun style -> function | Style f -> f style | _ -> style)
 
+        let mt, mr, ms, ml = 
+            match style.margin with 
+            | Uniform x -> x, x, x, x 
+            | Sides (t, r, s, l) -> t, r, s, l
         let x, y = x + style.margin, y + style.margin
         let width, height = width - (2 * style.margin), height - (2 * style.margin)
 
@@ -106,10 +118,10 @@ module Model =
         match element.elementType with
         | Row children -> 
             let renderImpl = fun left width child -> render newStyle (left, y) (width, height) child
-            renderRow renderImpl x width children
+            renderRow renderImpl x width width children
         | Column children -> 
             let renderImpl = fun top height child -> render newStyle (x, top) (width, height) child
-            renderCol renderImpl y height children
+            renderCol renderImpl y height height children
         | Text s -> renderText newStyle (x, y) (width, height) s
         | Button s -> renderButton newStyle (x, y) (width, height) s
 
