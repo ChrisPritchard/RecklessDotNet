e2a3bdfd107a74443b6e359d47a5736c83b4a617
Christopher <chrispritchard.nz@gmail.com>
2019-07-29 09:47:34 +1200

    fleshed out render function with a standin for render colour

--- lib/Xelmish.UI/Model.fs
+++ lib/Xelmish.UI/Model.fs
@@ -21,8 +21,8 @@ module Model =
         fontSize: float
         colour: Colour
         backgroundColour: Colour
-        margin: Sizes
-        padding: Sizes
+        margin: Size
+        padding: Size
         borderSize: int
         borderColour: Colour
         alignment: float
@@ -30,7 +30,6 @@ module Model =
     }
     and Colour = byte * byte * byte * byte
     and Size = Percent of float | Pixels of int
-    and Sizes = Uniform of Size | Sides of Size * Size * Size * Size
 
     let col attributes children = { elementType = Column children; attributes = attributes }
     let row attributes children = { elementType = Row children; attributes = attributes }
@@ -40,6 +39,9 @@ module Model =
     let onclick f = OnClick f
     let width i = Width i
     let height i = Height i
+
+    let px n = Pixels n
+    let pct n = Percent n
     
     let fontName s = Style (fun style -> { style with fontName = s })
     let fontSize s = Style (fun style -> { style with fontSize = s })
@@ -54,10 +56,12 @@ module Model =
 
     let testModel = 
         col [] [
-            text [ fontSize 20. ] "Cell 1"
+            text [ fontSize 20.; margin (px 10) ] "Cell 1"
             text [ ] "This is some sample text"
-            button [ onclick (fun _ -> ()) ] "Click Me"
-            button [ enabled false ] "Can't click Me"
+            row [ padding (pct 0.1) ] [
+                button [ onclick (fun _ -> ()) ] "Click Me"
+                button [ enabled false ] "Can't click Me"
+            ]
         ]
 
     let rec private renderRow renderImpl left totalSpace spaceRemaining childrenRemaining =
@@ -92,37 +96,50 @@ module Model =
                 yield! renderCol renderImpl (top + height) totalSpace (spaceRemaining - height) rest 
         ]
 
+    let private renderColour (x, y) (width, height) colour = []
+
     let private renderText style (x, y) (width, height) text = []
+
     let private renderButton style (x, y) (width, height) text = []
 
     let rec render style (x, y) (width, height) element = 
-        let newStyle = 
-            ({ style with padding = 0; margin = 0 }, element.attributes) 
-            ||> List.fold (fun style -> function | Style f -> f style | _ -> style)
+        [
+            let newStyle = 
+                ({ style with padding = px 0; margin = px 0 }, element.attributes) 
+                ||> List.fold (fun style -> function | Style f -> f style | _ -> style)
 
-        let mt, mr, ms, ml = 
-            match style.margin with 
-            | Uniform x -> x, x, x, x 
-            | Sides (t, r, s, l) -> t, r, s, l
-        let x, y = x + style.margin, y + style.margin
-        let width, height = width - (2 * style.margin), height - (2 * style.margin)
+            let topMargin, leftMargin = 
+                match style.margin with 
+                | Pixels n -> n, n 
+                | Percent p -> int (p * float height), int (p * float width)
+            let x, y = x + leftMargin, y + topMargin
+            let width, height = width - (2 * leftMargin), height - (2 * topMargin)
 
-        if style.borderSize > 0 then
-            () // render border
+            if style.borderSize > 0 then
+                yield! renderColour (x, y) (width, height) style.borderColour
 
-        // render background - border size
+            let x, y = x + style.borderSize, y + style.borderSize
+            let width, height = width - (2 * style.borderSize), height - (2 * style.borderSize)
+            yield! renderColour (x, y) (width, height) style.backgroundColour
 
-        let x, y = x + style.padding, y + style.padding
-        let width, height = width - (2 * style.padding), height - (2 * style.padding)
+            let topPadding, leftPadding = 
+                match style.padding with 
+                | Pixels n -> n, n 
+                | Percent p -> int (p * float height), int (p * float width)
+            let x, y = x + leftPadding, y + topPadding
+            let width, height = width - (2 * leftPadding), height - (2 * topPadding)
 
-        match element.elementType with
-        | Row children -> 
-            let renderImpl = fun left width child -> render newStyle (left, y) (width, height) child
-            renderRow renderImpl x width width children
-        | Column children -> 
-            let renderImpl = fun top height child -> render newStyle (x, top) (width, height) child
-            renderCol renderImpl y height height children
-        | Text s -> renderText newStyle (x, y) (width, height) s
-        | Button s -> renderButton newStyle (x, y) (width, height) s
+            match element.elementType with
+            | Row children -> 
+                let renderImpl = fun left width child -> render newStyle (left, y) (width, height) child
+                yield! renderRow renderImpl x width width children
+            | Column children -> 
+                let renderImpl = fun top height child -> render newStyle (x, top) (width, height) child
+                yield! renderCol renderImpl y height height children
+            | Text s -> 
+                yield! renderText newStyle (x, y) (width, height) s
+            | Button s -> 
+                yield! renderButton newStyle (x, y) (width, height) s
+        ]
 
     //let renderElements = render 
